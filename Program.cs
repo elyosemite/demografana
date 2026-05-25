299fb4eebebf2f192a2c0a55b59bf9e628e81f0a
Yuri Melo <yurifullstack@gmail.com>
2026-05-24 22:31:10 -0300

    Refactor Program.cs to configure AppDbContext and implement asynchronous order retrieval

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,11 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.AddObservability();
 
-builder.Services.AddSingleton<OrderStore>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<PlaceOrderUseCase>();
 builder.Services.AddScoped<GetOrdersUseCase>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -17,9 +26,8 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-
-app.MapGet("/orders", (GetOrdersUseCase useCase) =>
-    Results.Ok(useCase.Execute()))
+app.MapGet("/orders", async (GetOrdersUseCase useCase) =>
+    Results.Ok(await useCase.ExecuteAsync()))
     .WithName("GetOrders");
 
 app.MapPost("/orders", async (PlaceOrderRequest request, PlaceOrderUseCase useCase) =>
